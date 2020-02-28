30b788ef91d23fa396f2ba15000bf56530e55041
piettro <santana.piettrom@gmail.com>
2020-02-28 20:32:00 -0300

    exercicios pc1 cidades

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicios_cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");    
+}}}
